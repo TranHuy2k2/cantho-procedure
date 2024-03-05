95d3b0cdb420f1bf1f7cb69b1847385206770586
TranHuy2k2 <giahuytran2002@gmail.com>
2024-03-05 13:46:12 +0700

    chore: setup flutter router

---
+++ lib/router.dart
@@ -0,0 +1,15 @@
+import 'package:cantho_procedure/screens/home.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router/go_router.dart';
+
+// GoRouter configuration
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) =>
+          const MyHomePage(title: 'Dịch vụ công Cần Thơ'),
+    ),
+  ],
+);
